3788493504724d74de2f346944a3a4c7be903d7e
iberdinsky-skilld <iberdinsky@skilld.fr>
2018-08-17 13:52:07 +0600

    Notification Service Support.

---
+++ src/ios/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by Ivan Berdinsky on 16/08/2018.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
